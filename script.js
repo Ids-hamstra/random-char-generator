f9de546718fab3970d2c1b421931f948649e22e4
Ids-hamstra <idshamstra.github99@gmail.com>
2025-09-15 19:30:51 +0200

    Random class button works and displays on screen, added specs structure not finish yet

--- script.js
+++ script.js
@@ -4,18 +4,41 @@
 const CHARACTERS = ["Warrior", "Paladin", "Mage", "Shaman", "Monk",
      "Evoker", "Hunter", "Rogue", "Demon Hunter", "Priest", "Warlock",
     "Death Knight"];
-
-    function getRandomChar(characters) {
-        const randomIndexArray = Math.floor(Math.random() * characters.length);
-        return characters[randomIndexArray];
-       
-    }
     
-console.log(getRandomChar(CHARACTERS));
 
-function getRandomClass() {
+    const SPECS = {
+        Warrior: ["Arms", "Fury", "Paladin"],
+        Paladin: ["Holy", "Protection", "Retribution"],
+        Mage: ["Frost", "Arcane", "Fire"],
+        Shaman: ["Elemental", "Enhancement", "Restoration"],
+        Monk: ["Brewmaster", "Windwalker", "Mistweaver"],
+        Evoker: ["Devestation", "Preservation", "Augmentation"],
+        Hunter: ["Survival", "Markmanship", "Beast Mastery"],
+        Priest: ["Holy", "Discipline", "Shadow"],
+        Warlock: ["Destruction", "Demonology", "Affliction"],
+        DeathKnight: ["Frost", "Unholy", "Blood"]
+    };
 
-}
-   
+    
+
+ function getRandomChar() {
+        const randomIndexVanArray = Math.floor (Math.random() * CHARACTERS.length);
+        const RESULT = CHARACTERS[randomIndexVanArray];
+        document.getElementById("outputClass").textContent = RESULT;
+
+        console.log(RESULT);
+        return RESULT;
+     
+    };
+
+    // function getRandomSpec(getRandomChar) {
+    //     const SPECRANDOM = Math.floor(Math.random() * SPECS.length)
+    //     const RESULT = SPECS[SPECRANDOM]
+    //     document.getElementById("outputSpec").textContent = RESULT;
+    //     console.log(RESULT);
+    //     return RESULT
+    // }
+    
+
 
 
